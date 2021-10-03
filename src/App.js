823822c06d20537fb6526dc98568605ec6dfbba4
tal-bendet <tal.bendet@amdocs.com>
2021-10-03 18:51:30 +0300

    add

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from "react";
+import L from "leaflet";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "./app.css";
 import "leaflet/dist/leaflet.css";
 
-import { MapContainer, TileLayer } from "react-leaflet";
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: require("./images/marker-icon-2x.png"),
+  iconUrl: require("./images/marker-icon.png"),
+  shadowUrl: require("./images/marker-shadow.png"),
+});
 
 const App = () => {
   return (
@@ -12,6 +20,9 @@ const App = () => {
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         />
+        <Marker position={[31.253, 34.7915]}>
+          <Popup>Hello, Beer-sheva</Popup>
+        </Marker>
       </MapContainer>
     </div>
   );
